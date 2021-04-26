d2bd1f653fb4048231964b6aa7951a8d5c0ab115
Caius Eugene NHS <caius_hns@Caiuss-MacBook-Pro.local>
2021-04-26 14:45:01 +0100

    release

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// Generated using webpack-cli http://github.com/webpack-cli
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'venn.js',
+        library: 'venn',
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\\.(js|jsx)$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
